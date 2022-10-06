a0e0b7de8af05dbbbdaffe3b5cafe9a0fe657c77
EugeVel <93157547+EugeVel@users.noreply.github.com>
2022-10-06 10:55:58 +0200

    2 files modified

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('Hello!');
+// A $( document ).ready() block.
+$(document).ready(function () {
+  getUserData();
+});
+
+function getUserData() {
+  let randomNumber = Math.floor(Math.random() * 10);
+
+  console.log(randomNumber);
+
+  fetch('https://dummyjson.com/users/' + randomNumber)
+    .then((res) => res.json())
+    .then((user) => {
+      $('#user-name').text(user.firstName);
+
+      console.log(user);
+
+      let url = 'https://xsgames.co/randomusers/avatar.php?g=';
+
+      url = url + user.gender;
+
+      $('#user-image').prop('src', url);
+    });
+}
