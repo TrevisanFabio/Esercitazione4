7a98460491f13057579f192e175d56a27bba493f
EugeVel <93157547+EugeVel@users.noreply.github.com>
2022-10-07 09:10:41 +0200

    2 files modified

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('Hello!');
+$(document).ready(function () {
+  fetch('https://dummyjson.com/users')
+    .then((res) => res.json())
+    .then((data) => {
+      let users = data.users;
+      let usersTable = $('#usersTable');
+      for (user in users) {
+        console.log(users[user]);
+
+        let u = users[user];
+
+        let el = $(
+          '<tr><td>' + u.email + '</td><td>' + u.firstName + '</td></tr>'
+        );
+
+        usersTable.append(el);
+      }
+      $('#usersTable').DataTable();
+    });
+});
