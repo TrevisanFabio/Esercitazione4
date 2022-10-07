37691bf0f13d03c60d8ca5a0ab55043637d28f29
6190210 <77718703+6190210@users.noreply.github.com>
2022-10-07 09:43:47 +0200

    3 files modified

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log('Hello!');
+function getRandomLatLng(map) {
+  var bounds = map.getBounds(),
+    southWest = bounds.getSouthWest(),
+    northEast = bounds.getNorthEast(),
+    lngSpan = northEast.lng - southWest.lng,
+    latSpan = northEast.lat - southWest.lat;
+    
+  return new L.LatLng(
+      southWest.lat + latSpan * Math.random(),
+      southWest.lng + lngSpan * Math.random());
+}
