cec7ffadd1df2e61a0ae43c4720104c7b742da25
6190210 <77718703+6190210@users.noreply.github.com>
2022-10-10 16:58:17 +0200

    1 binaries uploaded

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ function getRandomLatLng(map) {
       southWest.lat + latSpan * Math.random(),
       southWest.lng + lngSpan * Math.random());
 }
+
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "user" && password === "web_dev") {
+        alert("You have successfully logged in.");
+        location.reload();
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
+
